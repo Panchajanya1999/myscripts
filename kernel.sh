f25a982c78776d51a852c413d7d99da92917be50
Panchajanya1999 <rsk52959@gmail.com>
2019-01-05 11:59:36 +0530

    script: kernel: Optimize Message loggings
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -35,7 +35,7 @@ function clone {
 	echo " "
 	echo "★★Cloning GCC Toolchain from Android GoogleSource .."
 	sleep 2
-	git clone --depth 1 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9.git
+	git clone --depth=1 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9.git
 	echo "★★GCC cloning done"
 	sleep 2
 	echo "★★Cloning Clang 7 sources (r328903)"
@@ -54,13 +54,11 @@ function exports {
 }
 
 function tg_post_msg {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHANNEL_ID" -d text="$1"
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$GROUP_ID" -d text="$1"
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" -d text="$1"
 }
 
 function tg_post_build {
-	curl -F chat_id="$CHANNEL_ID" -F document=@"$1" $BOT_BUILD_URL
-	curl -F chat_id="$GROUP_ID" -F document=@"$1" $BOT_BUILD_URL
+	curl -F chat_id="$2" -F document=@"$1" $BOT_BUILD_URL
 }
 
 function build_kernel {
@@ -73,7 +71,7 @@ function build_kernel {
 		DEFCONFIG=X00TD_defconfig
 	else
 		echo "Defconfig Mismatch"
-		tg_post_msg "☠☠Defconfig Mismatch..!! Build Failed..!!👎👎"
+		tg_post_msg "☠☠Defconfig Mismatch..!! Build Failed..!!👎👎" "$GROUP_ID"
 		echo "Exiting in 5 seconds"
 		sleep 5
 		exit
@@ -81,11 +79,11 @@ function build_kernel {
 	
 	make O=out $DEFCONFIG
 	BUILD_START=$(date +"%s")
-	tg_post_msg "★★Build Started on $(uname) $(uname -r)★★"
+	tg_post_msg "★★ Build Started on $(uname) $(uname -r) ★★" "$GROUP_ID"
 	make -j8 O=out \
 		CC=$KERNEL_DIR/clang-r328903/bin/clang \
 		CLANG_TRIPLE=aarch64-linux-gnu- \
-		CROSS_COMPILE=$KERNEL_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+		CROSS_COMPILE=$KERNEL_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android- 2>&1 | tee logcat.txt
 	BUILD_END=$(date +"%s")
 	BUILD_TIME=$(date +"%Y%m%d-%T")
 	DIFF=$(($BUILD_END - $BUILD_START))	
@@ -95,19 +93,19 @@ function check_img {
 	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ]
 	then 
 		echo -e "Kernel Built Successfully in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!"
-		tg_post_msg "👍👍Kernel Built Successfully in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!"
+		tg_post_msg "👍👍Kernel Built Successfully in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!" "$GROUP_ID"
 		gen_changelog
 		gen_zip
 	else 
 		echo -e "Kernel failed to compile after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!"
-		tg_post_msg "☠☠Kernel failed to compile after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!"
-		exit
+		tg_post_msg "☠☠Kernel failed to compile after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds..!!" "$GROUP_ID"
+		tg_post_build "logcat.txt" "$GROUP_ID"
 	fi	
 }
 
 function gen_changelog {
-	tg_post_msg "★★ChangeLog --
-	$(git log --oneline --decorate --color --pretty=%s --first-parent -7)"
+	tg_post_msg "★★ ChangeLog --
+	$(git log --oneline --decorate --color --pretty=%s --first-parent -7)" "$GROUP_ID"
 }
 
 function gen_zip {
@@ -116,8 +114,8 @@ function gen_zip {
 		echo "Zipping Files.."
 		mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
 		cd AnyKernel2
-		zip -r AzurE-X00TD-$BUILD_TIME * -x .git README.md
-		tg_post_build "AzurE-X00TD-$BUILD_TIME.zip"
+		zip -r9 AzurE-X00TD-$BUILD_TIME * -x .git README.md
+		tg_post_build "AzurE-X00TD-$BUILD_TIME.zip" "$GROUP_ID"
 		cd ..
 	fi
 }
