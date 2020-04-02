c658fce44342f439accbaefb9c863b63c7495eb0
Panchajanya1999 <panchajanya@azure-dev.live>
2020-04-02 20:11:48 +0530

    kernel: Refactor the whole Code
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,5 @@
- #
+#! /bin/sh
+
  # Script For Building Android arm64 Kernel
  #
  # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
@@ -16,11 +17,8 @@
  # limitations under the License.
  #
 
-#! /bin/sh
-
 #Kernel building script
 
-set -euo pipefail
 
 ##------------------------------------------------------##
 ##----------Basic Informations, COMPULSORY--------------##
@@ -46,7 +44,7 @@ INCREMENTAL=1
 
 # Push ZIP to Telegram. 1 is YES | 0 is NO(default)
 PTTG=1
-	if [ $PTTG == 1 ]
+	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
 		CHATID="-1001245830369"
@@ -62,7 +60,7 @@ BUILD_DTBO=1
 # Sign the zipfile
 # 1 is YES | 0 is NO
 SIGN=1
-	if [ $SIGN == 1 ]
+	if [ $SIGN = 1 ]
 	then
 			#Check java installed or not
 			JAVAEXEC="/usr/bin/java"
@@ -78,14 +76,15 @@ SIGN=1
 
 # Check if we are using a dedicated CI ( Continuous Integration ), and
 # set KBUILD_BUILD_VERSION and KBUILD_BUILD_HOST and CI_BRANCH
-if [ -v $CI ]
+if [ ! -z "$CI" ]
 then
-	if [ -v $CIRCLECI ]
+	if [ ! -z "$CIRCLECI" ]
 	then
 		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
 		export KBUILD_BUILD_HOST="CircleCI"
 		export CI_BRANCH=$CIRCLE_BRANCH
-	if [ -v $DRONE ]
+	fi
+	if [ -v "$DRONE" ]
 	then
 		export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
 		export KBUILD_BUILD_HOST=$DRONE_SYSTEM_HOST
@@ -94,6 +93,9 @@ then
 		echo "Not presetting Build Version"
 	fi
 fi
+
+#Check Kernel Version
+KERVER=$(make kernelversion)
 
 
 # Set a commit head
@@ -115,7 +117,7 @@ function clone {
 	echo "★★Clang Done, Now Its time for AnyKernel .."
 	git clone --depth 1 --no-single-branch https://github.com/Panchajanya1999/AnyKernel2.git -b $DEVICE
 	echo "★★Cloning libufdt"
-	git clone https://android.googlesource.com/platform/system/libufdt $KERNEL_DIR/scripts/ufdt/libufdt
+	git clone https://android.googlesource.com/platform/system/libufdt "$KERNEL_DIR"/scripts/ufdt/libufdt
 	echo "★★Cloning Kinda Done..!!!"
 }
 
@@ -125,7 +127,8 @@ function exports {
 	export KBUILD_BUILD_USER="panchajanya"
 	export ARCH=arm64
 	export SUBARCH=arm64
-	export KBUILD_COMPILER_STRING=$($TC_DIR/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	export KBUILD_COMPILER_STRING
 	PATH=$TC_DIR/bin/:$PATH
 	export PATH
 	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
@@ -146,37 +149,43 @@ function tg_post_msg {
 ##----------------------------------------------------------------##
 
 function tg_post_build {
-	curl --progress-bar -F document=@"$1" $BOT_BUILD_URL \
+	#Post MD5Checksum alongwith for easeness
+	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+	#Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
 	-F chat_id="$2"  \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
-	-F caption="$3"  
+	-F caption="$3%0A<b>MD5 Checksum : </b><code>$MD5CHECK</code>"  
 }
 
 ##----------------------------------------------------------##
 
 function build_kernel {
-	if [ $INCREMENTAL == 0 ]
+	if [ $INCREMENTAL = 0 ]
 	then
 		make clean && make mrproper && rm -rf out
 	fi
 
-	if [ "$PTTG" == 1 ]
+	if [ "$PTTG" = 1 ]
  	then
-		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
+		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
 	fi
 
 	make O=out $DEFCONFIG
-	if [ $DEF_REG == 1 ]
+	if [ $DEF_REG = 1 ]
 	then
 		cp .config arch/arm64/configs/$DEFCONFIG
 		git add arch/arm64/configs/$DEFCONFIG
 		git commit -m "$DEFCONFIG: Regenerate
-								This is an auto-generated commit"
+
+						This is an auto-generated commit"
 	fi
 
 	BUILD_START=$(date +"%s")
-	make -j$PROCS O=out \
+
+	make -j"$PROCS" O=out \
 		CROSS_COMPILE=aarch64-linux-gnu- \
 		CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
 		CC=clang \
@@ -186,57 +195,53 @@ function build_kernel {
 		OBJCOPY=llvm-objcopy \
 		OBJDUMP=llvm-objdump \
 		STRIP=llvm-strip 2>&1 | tee error.log
-	if [ $BUILD_DTBO == 1 ]
-	then
-		tg_post_msg "Building DTBO.." "$CHATID"
-		python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
-			create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
-	fi
-	BUILD_END=$(date +"%s")
-	DIFF=$((BUILD_END - BUILD_START))
-	check_img
-}
 
-##-------------------------------------------------------------##
+		BUILD_END=$(date +"%s")
+		DIFF=$((BUILD_END - BUILD_START))
 
-function check_img {
-	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ] 
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ] 
 	    then
-		gen_zip
-	else
-		if [ "$PTTG" == 1 ]
- 		then
-			tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+	    	if [ $BUILD_DTBO = 1 ]
+			then
+				tg_post_msg "<code>Building DTBO..</code>" "$CHATID"
+				python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
+			fi
+				gen_zip
+		else
+			if [ "$PTTG" = 1 ]
+ 			then
+				tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+			fi
 		fi
-	fi
+	
 }
 
 ##--------------------------------------------------------------##
 
 function gen_zip {
-	mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
-	if [ $BUILD_DTBO == 1 ]
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
+	if [ $BUILD_DTBO = 1 ]
 	then
-		mv $KERNEL_DIR/out/arch/arm64/boot/dtbo.img AnyKernel2/dtbo.img
+		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel2/dtbo.img
 	fi
-	cd AnyKernel2
-	zip -r9 $ZIPNAME-$DEVICE-$DATE * -x .git README.md
+	cd AnyKernel2 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DATE" * -x .git README.md
 
-	if [ $SIGN == 1 ]
+	if [ $SIGN = 1 ]
 	then
 		## Sign the zip before sending it to telegram
-		if [ "$PTTG" == 1 ]
+		if [ "$PTTG" = 1 ]
  		then
- 			tg_post_msg "Signing Zip file with AOSP keys.." "$CHATID"
+ 			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>" "$CHATID"
  		fi
 		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-$DATE.zip $ZIPNAME-$DEVICE-$DATE-signed.zip
+		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-"$DATE".zip $ZIPNAME-$DEVICE-"$DATE"-signed.zip
 	fi
 
-	MD5CHECK=$(md5sum $ZIPNAME-$DEVICE-$DATE-signed.zip | cut -d' ' -f1)
-	if [ "$PTTG" == 1 ]
+	if [ "$PTTG" = 1 ]
  	then
-		tg_post_build $ZIPNAME-$DEVICE-$DATE-signed.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
+		tg_post_build $ZIPNAME-$DEVICE-"$DATE"-signed.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 	fi
 	cd ..
 }
