18571b533cd09536545d4dfafe6c0e9c7379a7c9
Panchajanya1999 <panchajanya@azure-dev.live>
2020-04-09 02:50:42 +0530

    kernel: Fix naming the final zip for sharing
    Fix a bug which won't push the zip if it is not signed, because of different
    naming convention.
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel.sh
+++ kernel.sh
@@ -228,6 +228,9 @@ function gen_zip {
 	cd AnyKernel2 || exit
 	zip -r9 $ZIPNAME-$DEVICE-"$DATE" * -x .git README.md
 
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME-$DEVICE-"$DATE".zip"
+
 	if [ $SIGN = 1 ]
 	then
 		## Sign the zip before sending it to telegram
@@ -236,12 +239,13 @@ function gen_zip {
  			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>" "$CHATID"
  		fi
 		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-"$DATE".zip $ZIPNAME-$DEVICE-"$DATE"-signed.zip
+		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-"$DATE".zip $ZIP_FINAL-signed.zip
+		ZIP_FINAL="$ZIP_FINAL-signed.zip"
 	fi
 
 	if [ "$PTTG" = 1 ]
  	then
-		tg_post_build $ZIPNAME-$DEVICE-"$DATE"-signed.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+		tg_post_build $ZIP_FINAL "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 	fi
 	cd ..
 }
