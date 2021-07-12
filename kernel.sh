b248fed0c3b06bced423b2492fcd7ee86c341d82
Panchajanya1999 <panchajanya@azure-dev.live>
2021-07-12 10:52:40 +0530

    kernel: Switch to zipsigner-3.0-dexed from Magisk zipsigner
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel.sh
+++ kernel.sh
@@ -347,7 +347,7 @@ gen_zip() {
  			msg "|| Signing Zip ||"
 			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>"
  		fi
-		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
+		curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
 		java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
 		ZIP_FINAL="$ZIP_FINAL-signed"
 	fi
