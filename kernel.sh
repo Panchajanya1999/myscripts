0d2a26b4ab115f2d79b892fd25239af8a0c8f570
Panchajanya1999 <kernel@panchajanya.dev>
2022-08-06 11:21:22 +0530

    kernel: Import Neutron Clang-16
    Since I am using Archlinux Docker, using older clang built on glibc < 2.36
    is throwing errors. Switch to newer clang to avoid glibc errors.
    
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- kernel.sh
+++ kernel.sh
@@ -191,8 +191,8 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 	
 	if [ $COMPILER = "clang" ]
 	then
-		msg "|| Cloning Clang-14 ||"
-		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang-llvm
+		msg "|| Cloning Clang-16||"
+		git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git clang-llvm
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	fi
